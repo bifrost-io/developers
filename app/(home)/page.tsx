4faad4621e8307406ea34f5988d05ff1c16023b8
ZxStim <zxstim@gmail.com>
2025-12-10 11:23:13 +0700

    feat: updated contents

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,35 +4,25 @@ export default function HomePage() {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col items-center md:items-start lg:items-start justify-center h-[630px] px-8 md:px-16 lg:px-16">
-        <p className="px-4 py-2 mb-16 text-sm border-2 rounded-full dark:border-blue-400 border-blue-500 w-fit text-blue-500 dark:text-blue-400">
-          Một dự án cộng đồng bởi{" "}
-          <Link
-            href="https://zxstim.com"
-            target="_blank"
-            className="text-blue-500 dark:text-blue-400 underline hover:text-primary/80"
-          >
-            ZxStim
-          </Link>
+        <p className="px-4 py-2 mb-16 text-sm border-2 rounded-full dark:border-purple-500 border-purple-600 w-fit text-purple-600 dark:text-purple-400">
+          The crosschain staking layer
         </p>
         <h1 className="text-center text-3xl md:text-left md:text-5xl lg:text-left lg:text-7xl font-bold lg:w-2/3">
-          Bách khoa toàn thư về{" "}
-          <span className="text-blue-500 dark:text-blue-400">
-            tài chính phi tập trung
-          </span>
+          Bifrost Developer Documentation
         </h1>
         <div className="flex flex-row gap-4 mt-16">
           <Link
-            href="/tai-lieu"
+            href="/docs"
             className="px-8 py-3 dark:bg-[#ffffff] bg-[#000000] dark:text-[#000000] text-[#ffffff] rounded-full w-fit lg:text-3xl hover:dark:bg-[#ffffff]/80 hover:bg-[#000000]/80"
           >
-            Bắt đầu
+            Getting started
           </Link>
           <a
-            href="https://www.youtube.com/@defivn"
+            href="https://www.youtube.com/@bifrostio"
             target="_blank"
             className="px-8 py-3 dark:bg-gray-800 bg-gray-300 dark:text-white text-black rounded-full w-fit lg:text-3xl hover:dark:bg-gray-700 hover:bg-gray-400"
           >
-            Xem video
+            Videos
           </a>
         </div>
       </div>
